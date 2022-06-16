2ec8f86cd809280d5ec41fe569fe2f08502d00d1
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-06-16 13:25:40 +0100

    Replace os.path commands with pathlib

--- convert_history_to_python.py
+++ convert_history_to_python.py
@@ -2,29 +2,31 @@ import datetime
 import os
 import re
 import sys
+from pathlib import Path, PurePath
 
 
 def convert_history_to_python(data_dir, output_dir=None, qgis_user_profile_dir=None):
     if qgis_user_profile_dir is None:
         if sys.platform.startswith('win32'):
-            qgis_user_profile_dir = os.path.join(
+            qgis_user_profile_dir = Path(
                 os.environ.get('APPDATA'),
                 'QGIS/QGIS3/profiles/default'
             )
-            if not os.path.isdir(qgis_user_profile_dir):
-                qgis_user_profile_dir = os.path.join(
+            if not qgis_user_profile_dir.exists():
+                qgis_user_profile_dir = Path(
                     os.environ.get('LOCALAPPDATA'),
                     'QGIS/QGIS3/profiles/default'
                 )
         elif sys.platform.startswith('darwin'):
-            qgis_user_profile_dir = os.path.join(
+            qgis_user_profile_dir = Path(
                 '/Users', os.environ.get('USER'),
                 'Library/Application Support/QGIS/QGIS3/profiles/default'
             )
 
-    processing_log = os.path.join(qgis_user_profile_dir, 'processing/processing.log')
+    processing_log = Path(qgis_user_profile_dir, 'processing/processing.log')
+    print(processing_log)
 
-    if not os.path.isfile(processing_log):
+    if not processing_log.exists():
         print("Could not find processing log at ", processing_log)
         print("Please specify your QGIS Active Profile Folder as the second argument to this script.")
         print("(In QGIS go to 'Settings' > 'User Profiles' > 'Open Active Profile Folder' and copy the path.)")
@@ -45,14 +47,15 @@ def convert_history_to_python(data_dir, output_dir=None, qgis_user_profile_dir=N
 
     # FIXME: add this script directory (for now) or output_dir
     if not output_dir:
-        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outputs')
-    timestamped_dirname = os.path.join(output_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S'))
-    os.makedirs(timestamped_dirname, exist_ok=True)
-    output_file = os.path.join(timestamped_dirname, 'qgis_commands.py')
+        output_dir = Path(__file__).resolve().parent / 'outputs'
+    timestamped_dir = output_dir / datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
+    timestamped_dir.mkdir(parents=True, exist_ok=True)
+    output_file = timestamped_dir / 'qgis_commands.py'
 
     with open(output_file, 'w') as output:
         output.write(f"""
 import os
+from pathlib import Path
 from qgis import processing
 from qgis.core import QgsVectorLayer, QgsProject
 
@@ -81,15 +84,16 @@ def run(data_dir='{data_dir}'):
                     if output_matches:
                         current_output_file = output_matches.group(1)
                         if data_dir in current_output_file:
-                            current_output_file = current_output_file.replace(data_dir, '{0}')
-                            current_output_dir = os.path.dirname(current_output_file)
-                            output.write(f'    os.makedirs("{current_output_dir}".format(data_dir), exist_ok=True)\n')
+                            current_output_file = Path(current_output_file.replace(data_dir, '{0}'))
+                            current_output_dir = current_output_file.parent
+                            output.write(f'    output_dir = Path("{current_output_dir}".format(data_dir))\n')
+                            output.write(f'    output_dir.mkdir(parents=True, exist_ok=True)\n')
 
                     output.write(f'    {output_cmd}\n')
 
                     # If command outputs a shapefile, add it as a map layer
-                    if output_matches and current_output_file.endswith('.shp'):
-                        name = os.path.splitext(os.path.basename(current_output_file))[0]
+                    if output_matches and current_output_file.suffix == '.shp':
+                        name = Path(current_output_file.name).stem
                         output.write(f'    layer = QgsVectorLayer("{current_output_file}".format(data_dir), "{name}")\n')
                         output.write('    QgsProject.instance().addMapLayer(layer)\n')
 
@@ -108,12 +112,23 @@ if __name__ == "__main__":
         sys.exit(1)
 
     data_dir = sys.argv[1]
+    output_dir = None
     qgis_user_profile_dir = None
 
     if len(sys.argv) > 2:
-        output_dir = os.path.realpath(sys.argv[2])
+        output_dir = Path(sys.argv[2])
+        try:
+            output_dir.resolve(strict=True)
+        except FileNotFoundError:
+            print(f"Output dir {output_dir} does not exist.")
+            sys.exit(2)
 
     if len(sys.argv) > 3:
-        qgis_user_profile_dir = os.path.realpath(sys.argv[3])
+        qgis_user_profile_dir = Path(sys.argv[3])
+        try:
+            qgis_user_profile_dir.resolve(strict=True)
+        except FileNotFoundError:
+            print(f"User profile dir {qgis_user_profile_dir} does not exist.")
+            sys.exit(3)
 
     convert_history_to_python(data_dir, output_dir, qgis_user_profile_dir)
